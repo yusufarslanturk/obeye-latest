38d8587fc1811d001ef535f89f7947a808c9deb9
yusufarslanturk <75898525+yusufarslanturk@users.noreply.github.com>
2024-10-03 02:05:19 +0300

    Update and rename fonetyazilim.php to tbc.php

--- fonetyazilim.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-header("location:https://www.fonetyazilim.com",'_blank');
-
-exit;
-?>
-
-
---
+++ tbc.php
@@ -0,0 +1,8 @@
+<?php
+
+header("location:https://www.tbcteknoloji.com",'_blank');
+
+exit;
+?>
+
+
