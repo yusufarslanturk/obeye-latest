c47876bd81bd3bbf286ffae5baefead93f9a6797
yusufarslanturk <75898525+yusufarslanturk@users.noreply.github.com>
2024-10-03 01:55:50 +0300

    Update hosts.inc.php

--- include/hosts.inc.php
+++ include/hosts.inc.php
@@ -1,21 +1,5 @@
 <?php
 /*
-** Zabbix
-** Copyright (C) 2001-2020 Zabbix SIA
-**
-** This program is free software; you can redistribute it and/or modify
-** it under the terms of the GNU General Public License as published by
-** the Free Software Foundation; either version 2 of the License, or
-** (at your option) any later version.
-**
-** This program is distributed in the hope that it will be useful,
-** but WITHOUT ANY WARRANTY; without even the implied warranty of
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-** GNU General Public License for more details.
-**
-** You should have received a copy of the GNU General Public License
-** along with this program; if not, write to the Free Software
-** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
 
 
@@ -986,7 +970,7 @@ function isTemplate($hostId) {
  *           'template' => array(                   <- optional
  *               'value' => 'template-level value'
  *               'templateid' => 10001,
- *               'name' => 'Template OS Linux by SKYMON agent'
+ *               'name' => 'Template OS Linux by OBSEYE agent'
  *           ),
  *           'global' => array(                     <- optional
  *               'value' => 'global-level value'
@@ -1201,7 +1185,7 @@ function getInheritedMacros(array $hostids) {
  *           'template' => array(                   <- optional
  *               'value' => 'template-level value'
  *               'templateid' => 10001,
- *               'name' => 'Template OS Linux by SKYMON agent'
+ *               'name' => 'Template OS Linux by OBSEYE agent'
  *           ),
  *           'global' => array(                     <- optional
  *               'value' => 'global-level value'
